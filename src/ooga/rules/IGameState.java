6cd87f38a42fea0b1050c61c4fa7b384b1f72951
Tyler Jang <taj26@duke.edu>
2020-04-02 20:39:45 -0700

    added comments to rule interfaces

--- src/ooga/rules/IGameState.java
+++ src/ooga/rules/IGameState.java
@@ -4,6 +4,11 @@ public interface IGameState {
   //may be an enum
   //possible states include win, loss, continue, waiting for user input, invalid move
 
+  /**
+   * Retrieves the current String state as a result of a Move and its processing
+   *
+   * @return the current state
+   */
   String getState();
 
 }
