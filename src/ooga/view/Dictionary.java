ae539ffa8c18cc42fd5c8ee76f30bd580f0cb414
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-04-09 20:19:31 -0400

    finished RowMenu

--- src/ooga/view/Dictionary.java
+++ src/ooga/view/Dictionary.java
@@ -1,5 +1,54 @@
 package ooga.view;
 
-public class Dictionary {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.ResourceBundle;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyProperty;
+import javafx.beans.property.StringProperty;
 
+public class Dictionary {
+  private static final Dictionary INSTANCE = new Dictionary();
+  private static final String DEFAULT_LANGUAGE = "English";
+
+  private Map<String, ReadOnlyObjectWrapper<String>> myDictionary;
+  private Collection<String> myResourcePaths;
+  private String myLanguage;
+
+  public static Dictionary getInstance(){
+    return INSTANCE;
+  }
+
+  private Dictionary(){
+    myLanguage = DEFAULT_LANGUAGE;
+    myResourcePaths = new HashSet<>();
+    myDictionary = new HashMap<>();
+  }
+
+  public void addReference(String path){
+    myResourcePaths.add(path);
+    ResourceBundle newBundle = ResourceBundle.getBundle(path+"."+myLanguage);
+    updateWithResources(newBundle);
+  }
+
+  private void updateWithResources(ResourceBundle bundle){
+    for(String key : bundle.keySet()){
+      myDictionary.putIfAbsent(key,new ReadOnlyObjectWrapper<>());
+      myDictionary.get(key).set(bundle.getString(key));
+    }
+  }
+
+  public void setLanguage(String language){
+    myLanguage = language;
+    for(String path : myResourcePaths){
+      ResourceBundle bundle = ResourceBundle.getBundle(path+ "." + myLanguage);
+      updateWithResources(bundle);
+    }
+  }
+
+  public ReadOnlyProperty<String> get(String key){
+    return myDictionary.getOrDefault(key,null).getReadOnlyProperty();
+  }
 }
--- src/ooga/view/menu/RowMenu.java
+++ src/ooga/view/menu/RowMenu.java
@@ -1,5 +1,87 @@
 package ooga.view.menu;
 
-public class RowMenu {
+import java.awt.Color;
+import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
+import ooga.view.Dictionary;
 
+public class RowMenu implements Menu {
+  private static final double DEFAULT_SIZE= 500;
+  private static final Insets MARGINS = new Insets(20,20,20,20);
+  private static final double SPACING = 10;
+  private static final String CHOICES = "ooga.resources.languages.menu";
+  private static final ResourceBundle GAMES = ResourceBundle.getBundle("ooga.resources.languages.menu.English");
+
+  private BorderPane myPane;
+  private Stage myStage;
+
+  public RowMenu(){
+    Dictionary.getInstance().addReference(CHOICES);
+
+    HBox options = new HBox(SPACING);
+    myPane = new BorderPane();
+    myPane.setCenter(options);
+    options.setPadding(MARGINS);
+
+    myPane.setStyle("-fx-background-color: #CCFF99"); //this is just here so i can debug
+    options.prefWidthProperty().bind(myPane.widthProperty());
+    options.prefHeightProperty().bind(myPane.heightProperty());
+
+    for(String game : GAMES.keySet()){
+      addOption(game,options);
+    }
+
+    Pane dashboard = new Pane();
+    ComboBox<String> languages = new ComboBox<>();
+    languages.getItems().addAll("English","Español");
+    languages.valueProperty().addListener(new ChangeListener<String>() {
+      @Override
+      public void changed(ObservableValue<? extends String> observable, String oldValue,
+          String newValue) {
+        Dictionary.getInstance().setLanguage(newValue);
+      }
+    });
+    dashboard.getChildren().add(languages);
+    myPane.setTop(dashboard);
+
+    myStage = new Stage();
+    Scene scene = new Scene(myPane,DEFAULT_SIZE,DEFAULT_SIZE);
+    myStage.setScene(scene);
+  }
+
+  @Override
+  public void show() {
+    myStage.show();
+  }
+
+  @Override
+  public void addChosenHandler(ChangeListener<String> listener) {
+    //TODO
+  }
+
+  private void addOption(String key, HBox hbox){
+    Button option = new Button();
+    option.textProperty().bind(Dictionary.getInstance().get(key));
+    option.setMaxWidth(Double.MAX_VALUE);
+    HBox.setHgrow(option, Priority.ALWAYS);
+    option.prefHeightProperty().bind(hbox.heightProperty());
+    hbox.getChildren().add(option);
+  }
 }
