10c50c6ac96a8e4b20699a9f6152f1b4216ca4f2
Tyler Jang <taj26@duke.edu>
2020-04-19 21:34:04 -0700

    added comments to CardAction and ICardAction

--- src/ooga/data/rules/ICardAction.java
+++ src/ooga/data/rules/ICardAction.java
@@ -1,10 +1,18 @@
 package ooga.data.rules;
 
-
-import ooga.cardtable.IGameState;
 import ooga.cardtable.IMove;
 
+/**
+ * This interface refers to the movement and updates that will occur upon processing of a move.
+ *
+ * @author Tyler Jang
+ */
 public interface ICardAction { //TODO: UPDATE API CHANGES
 
-  void execute(IMove move);
+  /**
+   * Executes the ICardAction, moving the ICells based on move.
+   *
+   * @param move an IMove to be processed into ICell movements.
+   */
+    void execute(IMove move);
 }
