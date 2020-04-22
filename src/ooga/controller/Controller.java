290f61dd2731a1dc853508d35092e9212187b1f0
Tyler Jang <taj26@duke.edu>
2020-04-22 16:27:47 -0700

    made valid the default for now

--- src/ooga/controller/Controller.java
+++ src/ooga/controller/Controller.java
@@ -29,9 +29,9 @@ public class Controller extends Application {
     private static final String BACKUP_STYLE_FILE = "data/default_style_orig.xml";
 
     //private static final String DEFAULT_RULE_FILE = "data/solitaire_rules.xml";           //default
-    //private static final String DEFAULT_RULE_FILE = "data/solitaire_rules_static_1.xml";    //fixed cards, demo
+    private static final String DEFAULT_RULE_FILE = "data/solitaire_rules_static_1.xml";    //fixed cards, demo
     //private static final String DEFAULT_RULE_FILE = "data/solitaire_rules_static_2.xml";  //almost win state
-    private static final String DEFAULT_RULE_FILE = "data/solitaire_rules_static_3.xml";  //xsd error
+    //private static final String DEFAULT_RULE_FILE = "data/solitaire_rules_static_3.xml";  //xsd error
     //private static final String DEFAULT_RULE_FILE = "data/solitaire_rules_static_4.xml";  //runtime phase error
 
     private static final String WIN = "win";
