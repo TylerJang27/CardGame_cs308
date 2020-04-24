12354cfaac4887d0edbf0f4f5de54c7361effe40
Maverick Chung <mc608@duke.edu>
2020-04-24 16:02:06 -0700

    fixed 52card layout

--- data/52cardxml.py
+++ data/52cardxml.py
@@ -9,14 +9,14 @@ def generateLayout():
     for r in range(4):
         for c in range(13):
             print('<cell name="'+str(r*13+c)+'">')
-            print('<x_val>'+str(5+7*c)+'</x_val>')
-            print('<y_val>'+str(16+12*r)+'</y_val>')
+            print('<x_val>'+str(7+7*c)+'</x_val>')
+            print('<y_val>'+str(3+12*r)+'</y_val>')
             print('</cell>')
     r = 4
     c = 0
     print('<cell name="'+str(r*13+c)+'">')
-    print('<x_val>'+str(5+7*c)+'</x_val>')
-    print('<y_val>'+str(16+12*r)+'</y_val>')
+    print('<x_val>'+str(7+7*c)+'</x_val>')
+    print('<y_val>'+str(3+12*r)+'</y_val>')
     print('</cell>')
     
 def backerCards():
