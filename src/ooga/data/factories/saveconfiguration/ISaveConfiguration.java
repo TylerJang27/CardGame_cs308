7c8143281da00a33f4c42640d0ad3a05f3250be7
Tyler Jang <taj26@duke.edu>
2020-04-24 00:08:49 -0700

    created ISaveConfiguration and SaveConfiguration

---
+++ src/ooga/data/factories/saveconfiguration/ISaveConfiguration.java
@@ -0,0 +1,48 @@
+package ooga.data.factories.saveconfiguration;
+
+import ooga.cardtable.ICell;
+
+import java.util.Map;
+
+/**
+ * This interface governs saved configurations of games, including the game name, the current score, the current phase, and the current cell states.
+ *
+ * @author Tyler Jang
+ */
+public interface ISaveConfiguration {
+
+    /**
+     * Retrieves the name of the game that was stored.
+     *
+     * @return a String representing the game name
+     */
+    String getGameName();
+
+    /**
+     * Retrieves the file path of the rules.
+     *
+     * @return a String representing the path to the rules file
+     */
+    String getRulePath();
+
+    /**
+     * Retrieves the name of the current phase at save.
+     *
+     * @return a String representing the current phase
+     */
+    String getCurrentPhase();
+
+    /**
+     * Retrieves the current phase at save.
+     *
+     * @return a Double representing the score
+     */
+    Double getScore();
+
+    /**
+     * Retrieves the cell map present at save.
+     *
+     * @return a Map of String ICell names to ICells
+     */
+    Map<String, ICell> getCellMap();
+}
